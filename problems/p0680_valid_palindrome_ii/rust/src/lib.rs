25d5e85b0026eaeadb1eb2d9de3acdb369f641f7
Junyong Tan <jytan1701@gmail.com>
2026-05-20 21:54:03 -0500

    completed valid pali 2

--- problems/p0680_valid_palindrome_ii/rust/src/lib.rs
+++ problems/p0680_valid_palindrome_ii/rust/src/lib.rs
@@ -2,6 +2,28 @@ pub struct Solution;
 
 impl Solution {
     pub fn valid_palindrome(s: String) -> bool {
-        unimplemented!()
+        let b = s.as_bytes();
+        let (mut l, mut r) = (0usize, s.len().saturating_sub(1));
+
+        while l < r {
+            if b[r] != b[l] {
+                return is_pali(b, l + 1, r) || is_pali(b, l, r - 1);
+            }
+            l += 1;
+            r -= 1;
+        }
+
+        true
     }
+}
+
+fn is_pali(b: &[u8], mut l: usize, mut r: usize) -> bool {
+    while l < r {
+        if b[l] != b[r] {
+            return false;
+        }
+        l += 1;
+        r -= 1;
+    }
+    true
 }
