641743b66b84b6253e1c1a33ca8c0b88bc452cad
Junyong Tan <jytan1701@gmail.com>
2026-06-29 23:46:34 -0500

    redone two sum

--- problems/p0001_two_sum/rust/src/lib.rs
+++ problems/p0001_two_sum/rust/src/lib.rs
@@ -4,15 +4,15 @@ pub struct Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut seen: HashMap<i32, usize> = HashMap::new();
-        for (i, &n) in nums.iter().enumerate() {
-            let x = target - n;
-            if let Some(&j) = seen.get(&x) {
-                return vec![i as i32, j as i32];
-            }
-            seen.insert(n, i);
-        }
+        let mut hash_map: HashMap<i32, i32> = HashMap::new();
 
+        for (idx, n) in nums.iter().enumerate() {
+            let x = target - n;
+            if let Some(&j) = hash_map.get(&x) {
+                return vec![j, idx as i32];
+            }
+            hash_map.insert(*n, idx as i32);
+        }
         vec![]
     }
 }
