23b6053b2c5f1e4553b31bc41a6ca20037f0ed1a
Junyong Tan <jytan1701@gmail.com>
2026-07-01 22:36:54 -0500

    redone

--- problems/p0049_group_anagrams/rust/src/lib.rs
+++ problems/p0049_group_anagrams/rust/src/lib.rs
@@ -4,18 +4,18 @@ pub struct Solution;
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut hashmap: HashMap<String, Vec<String>> = HashMap::new();
+        let mut hash_map: HashMap<String, Vec<String>> = HashMap::new();
 
         for s in &strs {
-            let key = normalize(s);
-            hashmap.entry(key).or_default().push(s.clone());
+            hash_map.entry(normalize(s)).or_default().push(s.clone())
         }
-        hashmap.into_values().collect()
+
+        hash_map.into_values().collect()
     }
 }
 
-fn normalize(word: &str) -> String {
-    let mut bytes: Vec<u8> = word.bytes().collect();
-    bytes.sort();
-    String::from_utf8(bytes).unwrap()
+fn normalize(s: &str) -> String {
+    let mut chars: Vec<char> = s.chars().collect();
+    chars.sort_unstable();
+    chars.into_iter().collect()
 }
