0fda4f5adbf6052c5be60aa302289ee765fc0365
Junyong Tan <jytan1701@gmail.com>
2026-07-01 23:44:26 -0500

    redone 88

--- problems/p0088_merge_sorted_array/rust/src/lib.rs
+++ problems/p0088_merge_sorted_array/rust/src/lib.rs
@@ -2,19 +2,27 @@ pub struct Solution;
 
 impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        let mut i = m - 1;
-        let mut j = n - 1;
-        let mut k = m + n - 1;
+        let mut idx = m + n - 1;
+        let mut i1 = m - 1;
+        let mut i2 = n - 1;
 
-        while j >= 0 {
-            if i < 0 || nums1[i as usize] <= nums2[j as usize] {
-                nums1[k as usize] = nums2[j as usize];
-                j -= 1;
+        while idx >= 0 {
+            if i1 < 0 {
+                nums1[idx as usize] = nums2[i2 as usize];
+                i2 -= 1;
+            } else if i2 < 0 {
+                nums1[idx as usize] = nums1[i1 as usize];
+                i1 -= 1;
             } else {
-                nums1[k as usize] = nums1[i as usize];
-                i -= 1;
+                if nums1[i1 as usize] < nums2[i2 as usize] {
+                    nums1[idx as usize] = nums2[i2 as usize];
+                    i2 -= 1;
+                } else {
+                    nums1[idx as usize] = nums1[i1 as usize];
+                    i1 -= 1;
+                }
             }
-            k -= 1;
+            idx -= 1;
         }
     }
 }
