c88db5f616f67d295ad5a69c83d4cf8b0dfd8772
Junyong Tan <jytan1701@gmail.com>
2026-07-01 21:37:17 -0500

    redone 26

--- problems/p0026_remove_duplicates_from_sorted_array/rust/src/lib.rs
+++ problems/p0026_remove_duplicates_from_sorted_array/rust/src/lib.rs
@@ -2,17 +2,14 @@ pub struct Solution;
 
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-        if nums.is_empty() {
-            return 0;
-        }
-        let mut slow = 1usize;
+        let mut slow = 0_usize;
 
         for fast in 1..nums.len() {
-            if nums[fast] != nums[slow - 1] {
-                nums[slow] = nums[fast];
+            if nums[fast] != nums[slow] {
                 slow += 1;
+                nums[slow] = nums[fast]
             }
         }
-        slow as i32
+        (slow + 1) as i32
     }
 }
