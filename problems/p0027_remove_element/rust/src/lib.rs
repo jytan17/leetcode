d8df52008ca06d3afdc86b099c805e429b0fd674
Junyong Tan <jytan1701@gmail.com>
2026-07-01 22:07:05 -0500

    redone 27

--- problems/p0027_remove_element/rust/src/lib.rs
+++ problems/p0027_remove_element/rust/src/lib.rs
@@ -2,16 +2,14 @@ pub struct Solution;
 
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        let mut slow = 0;
-        let mut fast = 0;
-
-        while fast < nums.len() {
-            if nums[fast] != val {
-                nums.swap(slow, fast);
-                slow += 1;
+        let mut k = 0usize;
+        for i in 0..nums.len() {
+            if nums[i] != val {
+                nums.swap(i, k);
+                k += 1;
             }
-            fast += 1;
         }
-        slow as i32
+
+        k as i32
     }
 }
