2b219573e30d90e28fc5ca79afc915469cc6c440
Junyong Tan <jytan1701@gmail.com>
2026-07-13 21:48:22 -0500

    redone 169

--- problems/p0169_majority_element/rust/src/lib.rs
+++ problems/p0169_majority_element/rust/src/lib.rs
@@ -4,20 +4,17 @@ pub struct Solution;
 
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
-        let mut hash_map: HashMap<i32, u32> = HashMap::new();
+        let mut hash_map: HashMap<i32, i32> = HashMap::new();
         let mut ans = nums[0];
-        let mut best = 0u32;
+        let mut cnt = 0i32;
 
-        for &n in &nums {
-            let c = hash_map.entry(n).or_insert(0);
-            *c += 1;
-
-            if *c > best {
-                best = *c;
+        for n in nums {
+            *hash_map.entry(n).or_insert(0) += 1;
+            if hash_map.get(&n).unwrap() > &cnt {
                 ans = n;
+                cnt = *hash_map.get(&n).unwrap();
             }
         }
-
         ans
     }
 }
