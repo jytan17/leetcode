9066b647eb5cb8135f18348a80d37d4494781c25
Junyong Tan <jytan1701@gmail.com>
2026-09-07 11:25:19 -0500

    done 242

---
+++ neetcode/01_arrays_hashing/p0242_valid_anagram/DONE.md
@@ -0,0 +1,3 @@
+# 242. Valid Anagram
+
+Completed: 2026-09-07
--- neetcode/01_arrays_hashing/p0242_valid_anagram/solution.py
+++ neetcode/01_arrays_hashing/p0242_valid_anagram/solution.py
@@ -1,6 +1,7 @@
+from collections import Counter
 from typing import List, Optional
 
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        raise NotImplementedError
+        return Counter(s) == Counter(t)
