9c6bd2b6567ad1fea45c1a8f5e1ed81998882271
Junyong Tan <jytan1701@gmail.com>
2026-09-07 10:56:34 -0500

    done 271

---
+++ neetcode/01_arrays_hashing/p0271_encode_and_decode_strings/DONE.md
@@ -0,0 +1,3 @@
+# 271. Encode and Decode Strings
+
+Completed: 2026-09-07
--- neetcode/01_arrays_hashing/p0271_encode_and_decode_strings/solution.py
+++ neetcode/01_arrays_hashing/p0271_encode_and_decode_strings/solution.py
@@ -3,7 +3,26 @@ from typing import List, Optional
 
 class Solution:
     def encode(self, strs: List[str]) -> str:
-        raise NotImplementedError
+        out = []
+
+        for s in strs:
+            out.append(f"{len(s)}:{s}")
+
+        return "".join(out)
 
     def decode(self, s: str) -> List[str]:
-        raise NotImplementedError
+        out = []
+
+        idx = L = 0
+
+        while idx < len(s):
+            cur_char = s[idx]
+            if cur_char == ":":
+                start, end = idx + 1, idx + L + 1
+                out.append(s[start:end])
+                idx, L = end, 0
+            else:
+                L = (L * 10) + int(cur_char)
+                idx += 1
+
+        return out
