4c2146af422d51fa673ea7b866816b164c7edf13
Junyong Tan <jytan1701@gmail.com>
2026-09-09 21:56:19 -0500

    739 done

---
+++ neetcode/04_stack/p0739_daily_temperatures/DONE.md
@@ -0,0 +1,3 @@
+# 739. Daily Temperatures
+
+Completed: 2026-09-09
--- neetcode/04_stack/p0739_daily_temperatures/solution.py
+++ neetcode/04_stack/p0739_daily_temperatures/solution.py
@@ -1,6 +1,16 @@
+from math import inf
 from typing import List, Optional
 
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        raise NotImplementedError
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                _, prev_i = stack.pop()
+                result[prev_i] = i - prev_i
+            stack.append((t, i))
+
+        return result
