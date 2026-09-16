9924a659f2ee4ce792598ff4c689399a6cf4c4c2
Junyong Tan <jytan1701@gmail.com>
2026-09-15 21:58:55 -0500

    done 74

---
+++ neetcode/05_binary_search/p0074_search_a_2d_matrix/DONE.md
@@ -0,0 +1,3 @@
+# 74. Search a 2D Matrix
+
+Completed: 2026-09-15
--- neetcode/05_binary_search/p0074_search_a_2d_matrix/solution.py
+++ neetcode/05_binary_search/p0074_search_a_2d_matrix/solution.py
@@ -3,4 +3,26 @@ from typing import List, Optional
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        raise NotImplementedError
+        def binary_search(nums, target):
+            l, r = 0, len(nums)
+            while l < r:
+                m = (l + r) // 2
+                if nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m
+
+            return l < len(nums) and nums[l] == target
+
+        top, bot = 0, len(matrix) - 1
+
+        while top <= bot:
+            m = (top + bot) // 2
+            if matrix[m][0] <= target <= matrix[m][-1]:
+                return binary_search(matrix[m], target)
+            elif target < matrix[m][0]:
+                bot = m - 1
+            else:
+                top = m + 1
+
+        return False
