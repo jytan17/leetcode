75bd6e9b138e2e9d8172c502d7c1a73eb26f5398
Junyong Tan <jytan1701@gmail.com>
2026-09-16 21:53:54 -0500

    done

---
+++ neetcode/07_trees/p0104_maximum_depth_of_binary_tree/DONE.md
@@ -0,0 +1,3 @@
+# 104. Maximum Depth of Binary Tree
+
+Completed: 2026-09-16
--- neetcode/07_trees/p0104_maximum_depth_of_binary_tree/solution.py
+++ neetcode/07_trees/p0104_maximum_depth_of_binary_tree/solution.py
@@ -10,4 +10,7 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        raise NotImplementedError
+        if not root:
+            return 0
+        left, right = self.maxDepth(root.left), self.maxDepth(root.right)
+        return 1 + max(left, right)
