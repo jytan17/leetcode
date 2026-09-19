6cd02bc0d10929e421a3e636fa675b4d3d01cac0
Junyong Tan <jytan1701@gmail.com>
2026-09-18 20:00:18 -0500

    done

---
+++ neetcode/10_backtracking/p0078_subsets/DONE.md
@@ -0,0 +1,3 @@
+# 78. Subsets
+
+Completed: 2026-09-18
--- neetcode/10_backtracking/p0078_subsets/solution.py
+++ neetcode/10_backtracking/p0078_subsets/solution.py
@@ -3,4 +3,18 @@ from typing import List, Optional
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        raise NotImplementedError
+        self.ans = []
+
+        def bt(cur, idx):
+            if idx == len(nums):
+                self.ans.append(cur.copy())
+                return
+
+            cur.append(nums[idx])
+            bt(cur, idx + 1)
+            cur.pop()
+            bt(cur, idx + 1)
+
+        bt([], 0)
+
+        return self.ans
